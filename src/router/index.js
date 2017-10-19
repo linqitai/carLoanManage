52e192ed4b78e3c8b84b5b01c37aa0e6bee38b29
1434343424 <1434343424@qq.com>
2017-10-19 09:22:04 +0800

    143

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../pages/Login/Login'
 import Home from '../components/Home'
-import Mileage from '../pages/Mileage/Mileage'
+import Mileage from '../pages/Mileage/Mileage'0
 
 Vue.use(Router)
 
