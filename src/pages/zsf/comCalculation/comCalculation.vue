7c9fc2ebe910bbc127fa7cbd3ec1aaa7c467b7e9
1434343424 <1434343424@qq.com>
2017-11-09 13:57:22 +0800

    zz

--- src/pages/zsf/comCalculation/comCalculation.vue
+++ src/pages/zsf/comCalculation/comCalculation.vue
@@ -30,7 +30,7 @@
           <div class="element">
             <p class="inline">手机号</p>
             <div class="width140 inline">
-              <el-input type="number" v-model="mobile" placeholder="请输入手机号" class="input" :maxlength="maxlengthMobole"
+              <el-input type="number" v-model="mobile" placeholder="请输入手机号" class="input"
                         @keyup.enter.native="search"></el-input>
             </div>
           </div>
@@ -43,28 +43,27 @@
           <div class="element ">
             <p class="inline ">提升额度</p>
             <div class="width120 inline ">
-              <el-input type="number " v-model="quotaMin " placeholder="元 " class="input "></el-input>
+              <el-input type="number" v-model='quotaMin' placeholder="元 " class="input "></el-input>
             </div>
             <p class="inline ">至</p>
             <div class="width120 inline ">
-              <el-input type="number " v-model="quotaMax " placeholder="元 " class="input "></el-input>
+              <el-input type="number" v-model='quotaMax' placeholder="元 " class="input "></el-input>
             </div>
           </div>
           <div class="element" @click="search()">
             <el-button type="primary" class="searchBtn">查询</el-button>
           </div>
           <div class="element" @click="moreBtn">
-            <img class="moreIcon" src="../../../common/images/arrow_down.png" width="32" v-if="!searchCell">
-            <img class="moreIcon iconTransform" src="../../../common/images/arrow_down.png" width="32"
-                 v-if="searchCell">
+            <img class="moreIcon" src="../../../common/images/arrow_down.png" v-if="!searchCell">
+            <img class="moreIcon iconTransform" src="../../../common/images/arrow_down.png" v-if="searchCell">
           </div>
         </div>
         <!--第二行-->
-        <div class=" searchBox clear">
+        <div class=" searchBox clear" v-if="searchCell" >
           <div class="element">
             <p class="inline">房产价值</p>
             <div class="width120 inline">
-              <el-select v-model="house" placeholder="" @change="search">
+              <el-select v-model="house" placeholder="" @change="searchByHouseValue">
                 <el-option v-for="item in houseState" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
               </el-select>
@@ -73,7 +72,7 @@
           <div class="element">
             <p class="inline">车辆价值</p>
             <div class="width120 inline">
-              <el-select v-model="car" placeholder="" @change="search">
+              <el-select v-model="car" placeholder="" @change="searchByCarValue">
                 <el-option v-for="item in carState" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
               </el-select>
@@ -91,7 +90,7 @@
           <div class="element">
             <p class="inline">开门率</p>
             <div class="width120 inline">
-              <el-select v-model="openDoor" placeholder="" @change="search">
+              <el-select v-model="openDoor" placeholder="" @change="searchByOpenDoor">
                 <el-option v-for="item in openDoorState" :key="item.value" :label="item.label" :value="item.value">
                 </el-option>
               </el-select>
@@ -108,7 +107,7 @@
           </div>
         </div>
         <!--第三行-->
-        <div class=" searchBox clear">
+        <div class=" searchBox clear" v-if="searchCell">
           <div class="element">
             <p class="inline">客流统计</p>
             <div class="width120 inline">
@@ -146,13 +145,13 @@
           <th class="width60">手机号</th>
           <th class="width60">姓名</th>
           <th class="width60">本地房产</th>
-          <th class="width60">房产价值</th>
+          <th class="width60">房产价值(万元)</th>
           <th class="width70">有无车辆</th>
-          <th class="width70">车辆价值</th>
+          <th class="width70">车辆价值(万元)</th>
           <th class="width50">是否店铺所有者</th>
-          <th class="width50">店铺月营收</th>
+          <th class="width50">店铺月营收(万元)</th>
           <th class="width50">员工人数</th>
-          <th class="width50">店铺年租金</th>
+          <th class="width50">店铺年租金(万元)</th>
           <th class="width50">开业时长</th>
           <th class="width50">所处区域</th>
           <th class="width50">近三个月开门率</th>
@@ -160,46 +159,58 @@
           <th class="width50">客流统计</th>
           <th class="width50">银行流水</th>
           <th class="width50">征信报告</th>
-          <th class="width50">提升额度</th>
+          <th class="width50">提升额度(元)</th>
         </tr>
         </thead>
         <tbody>
-        <tr v-for="(item, index) in getList" :key="item.value" v-show="getList.length > 0">
-          <td class="width100">{{item.updated | getDateHM}}</td>
-          <td class="width50">{{item.zedAccount}}</td>
-          <td class="width180">
-            <el-tooltip :content="item.title" placement="right" effect="light">
-              <el-button size="mini" class="width180 ellipsis">{{item.title}}</el-button>
-            </el-tooltip>
-          </td>
-          <td class="width60">{{item.regDate}}</td>
-          <td class="width80">{{item.province}}{{item.city}}</td>
-          <td class="width90">{{item.mile}}</td>
-          <td class="width70">{{item.highPrice}}</td>
-          <td class="width50">{{item.name}}</td>
-          <td class="width100">{{item.carNum}}</td>
-          <td class="width50">{{item.mobile}}</td>
-          <td class="width50">{{item.pushPlatformType | getFormtype}}</td>
-          <td class="width50">{{item.applyStatus | getStatus}}</td>
-          <td class="width50">{{item.submitStatus | getSubmitState}}</td>
+        <tr v-for="item in getList" :key="item.id">
+          <td class="width180">{{item.date | getDateHM}}</td>
+          <td class="width60">{{item.mobile}}</td>
+          <td class="width60"></td>
+          <td class="width60">{{item.house | houseStatus}}</td>
+          <td class="width60">{{item.houseValue}}</td>
+          <td class="width70">{{item.car | carStatus}}</td>
+          <td class="width70">{{item.carValue}}</td>
+          <td class="width50">{{item.shop | shopStatus}}</td>
+          <td class="width50">{{item.monthlySales}}</td>
+          <td class="width50">{{item.employeeNum}}</td>
+          <td class="width50">{{item.annualRent}}</td>
+          <td class="width50">{{item.startBusinessTime | startYearStatus}}</td>
+          <td class="width50">{{item.area | areaStatus}}</td>
+          <td class="width50">{{item.openRate}}</td>
+          <td class="width50">{{item.openCashier}}</td>
+          <td class="width50">{{item.footfall}}</td>
+          <td class="width50">{{item.bankStatementFlag}}</td>
+          <td class="width50">{{item.creditReportFlag}}</td>
+          <td class="width50">{{item.promTotal}}</td>
         </tr>
         <tr v-show="getList.length === 0">
-          <td class="noData" colspan="12">暂无数据</td>
+          <td class="noData" colspan="19">暂无数据</td>
         </tr>
         </tbody>
       </table>
+      <div class="block" style="margin-top:10px" v-show="showPageTag">
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                       :current-page.sync="pageIndex" :page-size="pageSize" layout="total, prev, pager, next, jumper"
+                       :total="total">
+        </el-pagination>
+      </div>
     </div>
   </div>
   </div>
 </template>
 <script>
   import axios from 'axios'
-  import {format, getDate} from '../../../common/js/times'
+  import {format, getDate, getDateHM} from '../../../common/js/times'
   import {zsf, config} from '../../../api/index'
 
   export default {
     data() {
       return {
+        mobile: '',
+        name: '',
+        total: '',
+        searchCell: false,
         sdate: '',
         edate: '',
         house: '',
@@ -210,25 +221,30 @@
         people: '',
         bank: '',
         report: '',
-        getList: [],
+        getList: '',
+        pageIndex: 1,
+        pageSize: 16,
+        showPageTag: false,
+        quotaMin: '',
+        quotaMax: '',
         // 房子
         houseState: [{
           value: '',
           label: '全部'
         }, {
-          value: '0',
+          value: '0-0',
           label: '无'
         }, {
-          value: '0',
+          value: '0-100',
           label: '100万以下'
         }, {
-          value: '0',
+          value: '100-300',
           label: '100万-300万'
         }, {
-          value: '0',
+          value: '300-800',
           label: '300万-800万'
         }, {
-          value: '0',
+          value: '800',
           label: '800万以上'
         }],
         // 车
@@ -236,126 +252,201 @@
           value: '',
           label: '全部'
         }, {
-          value: '0',
+          value: '0-0',
           label: '无'
         }, {
-          value: '0',
+          value: '0-20',
           label: '20万以下'
         }, {
-          value: '0',
+          value: '20-50',
           label: '20万-50万'
         }, {
-          value: '0',
+          value: '50-100',
           label: '50万-100万'
         }, {
-          value: '0',
+          value: '100',
           label: '100万以上'
         }],
         // 是否店铺所有者
         shopOwnerState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
           value: '0',
           label: '是'
         }, {
-          value: '0',
+          value: '1',
           label: '否'
         }],
         // 开门率
         openDoorState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
-          value: '0',
+          value: '0.9',
           label: '≥90%'
         }, {
-          value: '0',
+          value: '0.8-0.9',
           label: '80%-90%'
         }, {
-          value: '0',
+          value: '0.7-0.8',
           label: '70%-80%'
         }, {
-          value: '0',
+          value: '0-0.7',
           label: '≤70%'
         }],
         // 臻收银
         collectMoneyState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
           value: '0',
           label: '有'
         }, {
-          value: '0',
+          value: '1',
           label: '无'
         }],
         // 客流统计
         peopleState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
           value: '0',
           label: '有'
         }, {
-          value: '0',
+          value: '1',
           label: '无'
         }],
         // 银行流水
         bankState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
           value: '0',
           label: '有'
         }, {
-          value: '0',
+          value: '1',
           label: '无'
         }],
         // 统计报告
         reportState: [{
-          value: '0',
+          value: '',
           label: '全部'
         }, {
           value: '0',
           label: '有'
         }, {
-          value: '0',
+          value: '1',
           label: '无'
         }]
       }
     },
     created() {
+      this.search();
     },
     filters: {
-//      carState(t)
+      getDateHM(t) {
+        return getDateHM(t)
+      },
+      getDate(t) {
+        return getDate(t)
+      },
+      houseStatus(t) {
+        return t === 1 ? '有' : t === 0 ? '无' : ''
+      },
+      carStatus(t) {
+        return t === 1 ? '有' : t === 0 ? '无' : ''
+      },
+      shopStatus(t) {
+        return t === 1 ? '有' : t === 0 ? '无' : ''
+      },
+      startYearStatus(t) {
+        return t === 0 ? '一年以内' : t === 1 ? '3年以内' : t === 2 ? '3年以上' : ''
+      },
+      areaStatus(t) {
+        return t === 0 ? '郊区工业区' : t === 1 ? '郊区住宅区' : t === 2 ? '郊区商业区' : t === 3 ? '城区住宅区' : t === 4 ? '城区商业区' : ''
+      }
     },
     methods: {
       search() {
         let params = {
-//          startDate: this.sdate ? getDate(this.sdate) : '',
-//          endDate: this.edate ? getDate(this.edate) : '',
-          mobile: this.mobile
-        //  name: this.name,               //
-      //    fallfoot: this.people,               // 客流统计
-      //    shop: this.shopOwner,                    // 有无店铺
-      //    cashier: this.collectMoney,                // 臻收银
-//          quotaMin:                // 提升额度最小值
-//          quotaMax:
-       //   startDate: this.sdate,
-       //   endDate: this.edate,
-//          carValueMin:
-//          carValueMax:
-//          houseValueMin:
-//          houseValueMax:
-//          openDoorRateMin:
-//          openDoorRateMax:
-       //   creditReportFlag: this.report,       // 征信报告
-        //  bankStatementFlag: this.bank             // 银行流水
+          mobile: this.mobile,
+          page: this.pageIndex,
+          size: this.pageSize,
+          startDate: this.sdate ? getDate(this.sdate) : '',
+          endDate: this.edate ? getDate(this.edate) : '',
+          name: this.name,
+          quotaMin: this.quotaMin,
+          quotaMax: this.quotaMax,
+          houseValueMin: this.houseValueMin,
+          houseValueMax: this.houseValueMax,
+          carValueMin: this.carValueMin,
+          carValueMax: this.carValueMax,
+          openDoorRateMin: this.openDoorRateMin,
+          openDoorRateMax: this.openDoorRateMax,
+          shop: this.shopOwner,
+          cashier: this.collectMoney,
+          fallfoot: this.people,
+          bankStatementFlag: this.bank,
+          creditReportFlag: this.report
+
         }
         axios.post(zsf + '/promoteQuota/queryForWeb', params, config).then(res => {
-
+          if (res.data.code === 1000) {
+            // this.getList = res.data.data.result ? res.data.data.result : '';
+            this.$nextTick(() => {
+              this.getList = res.data.data.result ? res.data.data.result : ''
+              this.total = res.data.data.total
+              if (this.total <= this.pageSize) {
+                this.showPageTag = false
+              } else {
+                this.showPageTag = true
+              }
+            })
+          }
         })
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`)
+      },
+      handleCurrentChange(val) {
+        console.log(val)
+        this.pageIndex = val
+        this.search()
+      },
+      searchByHouseValue() {
+        let price = this.house
+        console.log(price)
+        this.houseValueMin = price.split('-')[0]
+        this.houseValueMax = price.split('-')[1]
+        this.search()
+      },
+      searchByCarValue() {
+        let price = this.car
+        console.log(price)
+        this.carValueMin = price.split('-')[0]
+        this.carValueMax = price.split('-')[1]
+        this.search()
+      },
+      searchByOpenDoor() {
+        let price = this.openDoor
+        console.log(price)
+        this.openDoorRateMin = price.split('-')[0]
+        this.openDoorRateMax = price.split('-')[1]
+        this.search()
+      },
+      moreBtn() {
+        this.searchCell = !this.searchCell
+      },
+      startTimeChange() {
+        if (this.edate) {
+          this.search()
+        }
+      },
+      endTimeChange() {
+        if (this.sdate) {
+          this.search()
+        }
       }
     }
   }
