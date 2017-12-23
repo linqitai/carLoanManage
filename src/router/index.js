69ee086ee45181f30746bfcbfb442a947e9d9c20
anjingxiaogongzhu <15267085944@163.com>
2017-12-23 09:34:29 +0800

    请忽略，无效提交

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,15 @@ export default new Router({
         },
         {
           path: '/bill',
-          component: resolve => require(['../pages/zsy/financialManagement/billingArrangement.vue'], resolve)
+          component: resolve => require(['../pages/zsy/financialManagement/billingArrangement.vue'], resolve),
+          children: [
+            {
+              path: 'details',
+              name: '详情',
+              meta: {path: '/bill'},
+              component: resolve => require(['../pages/zsy/financialManagement/details.vue'], resolve)
+            }
+          ]
         },
         {
           path: '/channelReconciliation',
@@ -87,10 +95,6 @@ export default new Router({
         {
           path: '/fenRunIncome',
           component: resolve => require(['../pages/zsy/financialManagement/fenRunIncome.vue'], resolve)
-        },
-        {
-          path: '/details',
-          component: resolve => require(['../pages/zsy/financialManagement/details.vue'], resolve)
         }
       ]
     },
